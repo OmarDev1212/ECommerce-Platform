974cb10cf8d7fa7a6c68c5b8589d98640eb67fe6
3omar63 <om728526@gmail.com>
2025-05-04 18:00:30 +0300

    GenericRepository

---
+++ Core/DomainLayer/Contracts/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using DomainLayer.Models;
+using DomainLayer.Models.ProductModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contracts
+{
+    public interface IGenericRepository<TEntity, Tkey> where TEntity : BaseEntity<Tkey>
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> GetById(Tkey id);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+
+    }
+}
---
+++ Infrastrcture/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class GenericRepository<TEntity, Tkey>(StoreDbContext _dbContext) : IGenericRepository<TEntity, Tkey> where TEntity : BaseEntity<Tkey>
+    {
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbContext.AddAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbContext.Remove(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _dbContext.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity> GetById(Tkey id)
+        {
+            return await _dbContext.FindAsync<TEntity>(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Update(entity);
+        }
+    }
+}

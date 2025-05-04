2b7eb25b04c7dd2b781a7b8dc1ce21dbb9612eef
3omar63 <om728526@gmail.com>
2025-05-04 18:26:22 +0300

    added IProductService and Dtos

---
+++ Core/ServiceAbstractions/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using Shared.DTO.ProductModule;
+
+namespace ServiceAbstractions
+{
+    public interface IProductService
+    {
+        IEnumerable<ProductDto> GetAllProducts();
+        ProductDto GetProductById(int id);
+        IEnumerable<BrandDto> GetBrands();
+        IEnumerable<TypeDto> GetTypes();
+    }
+}

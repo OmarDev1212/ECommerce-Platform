861b74e055f825c1729892dea87554752883d0af
3omar63 <om728526@gmail.com>
2025-05-25 03:43:05 +0300

    created order entities ,configurations  and updated database

---
+++ Core/DomainLayer/Models/OrderAggregate/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.OrderAggregate
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; }
+        public OrderStatus Status { get; set; }
+        public ShippingAddress Address { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public int DeliveryMethodId { get; set; }//[FK]
+        public ICollection<OrderItem> Items { get; set; } = [];
+        public decimal SubTotal { get; set; }
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Price;
+    }
+}
